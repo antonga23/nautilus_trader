454b2fa1605f315203b5bfbd7bce84df5532b3f2
Alatha <alatha@datatracking.space>
2023-11-28 11:26:27 +0200

    feat(ci): added test job to .gitlab-ci.yml - Enable common cache across jobs with additional paths and expiry - Add test-job with conditional execution for cloudbet branch and junit report generation - Update test-cloudbet.sh to generate junit report

--- scripts/test-cloudbet.sh
+++ scripts/test-cloudbet.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 poetry install --with test --all-extras
-poetry run pytest tests/integration_tests/adapters/cloudbet --new-first --failed-first
+poetry run pytest tests/integration_tests/adapters/cloudbet --new-first --failed-first --junitxml=report.xml
